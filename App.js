019508f3f814d58c7d5c1e48876b7e0849cfb1f5
saananokela <smnokela@gmail.com>
2024-08-21 13:00:23 +0300

    Implementation of BMI calculator app.

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+
 
 export default function App() {
+  const [weight, setWeight] = useState('')
+  const [height, setHeight] = useState('')
+  const [bmi, setBmi] = useState(null)
+
+  const calculateBmi = () => {
+    // korvataan mahdollinen pilkku pisteeksi
+    const weightNum = parseFloat(weight.replace(',' , '.'));
+    const heightNum = parseFloat(height.replace(',' , '.'));
+
+    console.log("Paino: ", weightNum);
+    console.log("Pituus: ", heightNum);
+
+    if (weightNum > 0 && heightNum > 0) {
+      const bmiValue = weightNum / (heightNum * heightNum);
+      setBmi(bmiValue.toFixed(1));
+    } else {
+      setBmi(null); //jos syöte on virheellinen
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Paino (kg)</Text>
+      <TextInput
+        placeholder="Kirjoita painosi tähän"
+        value={weight}
+        onChangeText={text => setWeight(text)}
+        keyboardType='numeric'
+        style={styles.input}
+      />
+      <Text>Pituus (m)</Text>
+      <TextInput
+        placeholder="Kirjoita pituutesi tähän"
+        value={height}
+        onChangeText={text => setHeight(text)}
+        keyboardType='numeric'
+        style={styles.input}
+      />
+      <Text>BMI</Text>
+      <Button
+        title='Laske painoindeksi'
+        onPress={calculateBmi}
+      />
+      {bmi !== null && (
+        <Text>BMI: {bmi}</Text>
+      )}
+      {/* <StatusBar style="auto" /> */}
     </View>
   );
 }
@@ -17,4 +62,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginVertical: 10,
+    width: 200,
+    textAlign: 'center',
+  }
 });
